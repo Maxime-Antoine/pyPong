078469d66b7b37971be02d3fd4aa9534ab47cbf6
Maxime Antoine <maxime.antoine2@gmail.com>
2017-09-24 17:48:58 +0800

    Add ball

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
+from random import randint
 from kivy.app import App
 from kivy.uix.widget import Widget
-
-
-class PongGame(object):
-    pass
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
 
 
 class PongApp(App):
-    def Build(self):
-        return PongGame()
+
+    def build(self):
+        game = PongGame()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
+
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 
 if __name__ == "__main__":
