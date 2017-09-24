04d5702fdf7d005839b32910db64de591f747a14
Maxime Antoine <maxime.antoine2@gmail.com>
2017-09-24 18:55:08 +0800

    Added paddles and scoring

--- main.py
+++ main.py
@@ -15,34 +15,62 @@ class PongApp(App):
         return game
 
 
-
 class PongGame(Widget):
     ball = ObjectProperty(None)
-    
-    def update(self, dt):
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+
+    def update(self, _):
         self.ball.move()
 
         # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
 
-        # bounce off left and right
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
-    
-    def serve_ball(self):
+        # bounce of paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+        # went of to a side to score point?
+        if self.ball.x < 0:
+            self.player2.score += 1
+            self.serve_ball(velocity=(4, 0))
+        if self.ball.x > self.width:
+            self.player1.score += 1
+            self.serve_ball(velocity=(-4, 0))
+
+    def serve_ball(self, velocity=(4, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = velocity
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width / 3:
+            self.player2.center_y = touch.y
 
 
 class PongBall(Widget):
+
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
-
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
+
+
+class PongPaddle(Widget):
+
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
 
 
 if __name__ == "__main__":
